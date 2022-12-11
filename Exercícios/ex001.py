41544f62674ec88908e4a23ecf9b0184685c123a
marcelo-moreira-code <contatomarcelomoreira.code@gmail.com>
2022-12-11 18:24:32 -0300

    Fiz nada
    nadinha

--- Exercícios/ex001.py
+++ Exercícios/ex001.py
@@ -1,6 +1,6 @@
 # Exercício 001 - Crie um programa que escreva 'Olá, mundo' na tela.
 
-#msg = 'Olá , mundo!'
-#print(msg)
-msg = ('\033[34m Olá , mundo! \033[m')
+msg = 'Olá , mundo!'
 print(msg)
+#msg = ('\033[34m Olá , mundo! \033[m')
+#print(msg)
--- Exercícios/ex002.py
+++ Exercícios/ex002.py
@@ -1,4 +1,4 @@
-# Exercício 002 - Faça um programa que #leia o nome de uma pessoa e mostre um #mensagem de boas-vindas.
+# Exercício 002 - Faça um programa que #leia o nome de uma pessoa e mostre um #mensagem de # boas-vindas.
 
 nome = str(input('Informe o nome: '))
 msg = (f'Olá {nome}, muito prazer em ti conhecer!')

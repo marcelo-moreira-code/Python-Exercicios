4f237e93a82cb8dc96f0182f3acdd7bc4f96d53c
marcelo-moreira-code <contatomarcelomoreira.code@gmail.com>
2022-12-12 08:59:50 -0300

    Ex003 feito
    resolvido 003

--- Exercícios/ex003.py
+++ Exercícios/ex003.py
@@ -1,2 +1,5 @@
-nome = input('Informe um nome: ')
-print(f'Olá {nome}, muito prazer em conhecê-la(o)')
+# Exercício 003 - Crie um programa que leia #dois números e mostre a soma entre eles.
+
+num1 = float(input('Informe o primeiro número: '))
+num2 = float(input('Agora o segundo: '))
+print(f'A soma entre os números {num1} e {num2} é {num1+num2}')
---
+++ Exercícios/ex004.py
@@ -0,0 +1,4 @@
+# Exercício 004 - Faça um programa que leia algo na tela e #mostre seu tipo primitivo e todas as informações sobre ele.
+
+algo = input('Digite algo: ')
+print(f'o tipo primitivo de {algo} é {type(algo)}')

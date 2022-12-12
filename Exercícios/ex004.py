21cc8e965e21383cc635f43aaa74a008524816e2
marcelo-moreira-code <contatomarcelomoreira.code@gmail.com>
2022-12-12 12:12:46 -0300

    update ex004
    nada

--- Exercícios/ex004.py
+++ Exercícios/ex004.py
@@ -1,4 +1,8 @@
-# Exercício 004 - Faça um programa que leia algo na tela e #mostre seu tipo primitivo e todas as informações sobre ele.
+# Exercício 004 - Faça um programa que leia algo na tela e 
+# mostre seu tipo primitivo e todas as informações sobre ele.
 
 algo = input('Digite algo: ')
-print(f'o tipo primitivo de {algo} é {type(algo)}')
+print(f'O tipo primitivo de {algo} é {type(algo)}')
+print(f'{algo} é numérico? {algo.isnumeric}')
+print(f'{algo} é formado somente por letras? {algo.isalpha}')
+print(f'{algo} é formado por elementos numéricos e letras? {algo.isalnum}')

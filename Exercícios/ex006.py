5e9405b0169a887d5624423d9b6b47a06171351b
marcelo-moreira-code <contatomarcelomoreira.code@gmail.com>
2022-12-12 23:38:23 -0300

    Resolvi os ex 5,6,7
    Feitos

---
+++ Exercícios/ex006.py
@@ -0,0 +1,4 @@
+# Exercício 006 - Crie um programa leia um número e mostre o seu dobro , triplo e sua raíz quadrada.
+
+num = int(input('Escreva um número inteiro: '))
+print(f'Para o número {num}, temos como:\nO dobro {num*2}\nO triplo é {num*3}\nE por fim, a sua raíz quadrada é {num**(1/2)}')
---
+++ Exercícios/ex007.py
@@ -0,0 +1,6 @@
+# Faça um programa que calcule duas notas dele , e mostre a média do aluno.
+
+nota1 = float(input('Informa a primeira nota: '))
+nota2 = float(input('A segunda nota: '))
+média = (nota1+nota2)/2
+print(f'Para um aluno que obteve as notas {nota1} e {nota2} , sua média será {média}')

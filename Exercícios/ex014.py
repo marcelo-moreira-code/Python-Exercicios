2bbbc6e460ba389f3b6905138c22aec2d1dd3a62
marcelo-moreira-code <contatomarcelomoreira.code@gmail.com>
2022-12-16 12:46:13 -0300

    Update ex014.py
    ex014 colorido

--- Exercícios/ex014.py
+++ Exercícios/ex014.py
@@ -1,3 +1,8 @@
-# Desafio 014 - Faça um programa que converta um temperatura digitada em C° para F°.
+# Desafio 014 - Faça um programa que converta uma temperatura digitada em C° para F°.
 
-tc = 5*((tf - 32)/9)
+# tc/5 = (tf-32)/9 -> tf = (tc/5) * 9 + 32
+
+temperatura_celcius = float(input('Informe a temperatura em C°: '))
+temperatura_farheirait = (temperatura_celcius * 9)/5 + 32
+
+print(f'\033[34m{temperatura_celcius} C°\033[m convertido para Farheirait fica \033[31m{temperatura_farheirait} F°\033[m')

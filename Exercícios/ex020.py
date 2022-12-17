528871b8cbec021d07367c97e1792ee7d7d291bf
marcelo-moreira-code <contatomarcelomoreira.code@gmail.com>
2022-12-17 09:10:40 -0300

    Update ex020.py
    feito

--- Exercícios/ex020.py
+++ Exercícios/ex020.py
@@ -1,3 +1,13 @@
 # Desafio 020 - O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalhos dos alunos.
 # Faça um progama que leia o nome dos quatro alunos e mostre a ordem sorteada.
 
+import random
+lista_nomes = []
+for nome in range(1,5):
+    lista_nomes.append(str(input(f'Informe o nome do {nome}ª aluno: ')))
+
+print(lista_nomes)
+
+
+random.shuffle(lista_nomes)
+print(lista_nomes)

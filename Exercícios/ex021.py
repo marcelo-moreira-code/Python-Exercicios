4da53f2129bfd884dde615dce8eda23bf7d0c974
marcelo-moreira-code <contatomarcelomoreira.code@gmail.com>
2022-12-17 09:35:14 -0300

    updated ex021
    deu ruim no VSC
    pego no Pycharm

--- Exercícios/ex021.py
+++ Exercícios/ex021.py
@@ -1,2 +1,10 @@
 # Desafio 021 - Faça um programa em Python que abra e reproduza o áudio de um arquivo em mp3.
 
+'''from playsound import playsound
+
+print(playsound('braces.mp3'))'''
+
+import os
+
+música = 'braces.mp4'
+os.system(música)

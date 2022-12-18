a510b6c69f4c8bcb54392d92d49af01baeb2c49d
marcelo-moreira-code <contatomarcelomoreira.code@gmail.com>
2022-12-18 19:23:23 -0300

    Create ex030.py
    feito

---
+++ Exercícios/ex030.py
@@ -0,0 +1,7 @@
+# Desafio 030 - Crie um programa que mostre um número na tela e diga se ele é par ou ímpar.
+
+num = int(input('Digite um número: '))
+if num % 2 == 0:
+    print('É par!')
+else:
+    print('É ímpar!')    

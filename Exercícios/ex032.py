0a61e1bf3dbb7df3ee7aadafc0402d5996519573
marcelo-moreira-code <contatomarcelomoreira.code@gmail.com>
2022-12-19 11:13:19 -0300

    Update ex032.py
    feito **

--- Exercícios/ex032.py
+++ Exercícios/ex032.py
@@ -1,10 +1,7 @@
 # Desafio 032 - Faça um programa que leia um ano qualquer e e mostre se ele  é bissexto.
 
-from datetime import date
-atual = date.today().year
-print(atual)
-
 ano = int(input('Informe o ano a ser analisado: '))
+
 if (ano % 4 == 0 and ano % 100 != 0) or  (ano % 4 == 0 and ano % 400 == 0) :
     print('É ano Bissexto!')
 else:

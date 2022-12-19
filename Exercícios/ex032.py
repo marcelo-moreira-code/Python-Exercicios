56fef8aca4e854b806ea3f98ec7a274a1d4d4d06
marcelo-moreira-code <contatomarcelomoreira.code@gmail.com>
2022-12-19 11:19:51 -0300

    Update ex032.py
    feito **

--- Exercícios/ex032.py
+++ Exercícios/ex032.py
@@ -1,5 +1,7 @@
 # Desafio 032 - Faça um programa que leia um ano qualquer e e mostre se ele  é bissexto.
 
+# Para ser ano bissexto têm que ser : divisível por 4 e não divisível por 100 ; (mas , tem de ser divisível por 400?) 
+
 ano = int(input('Informe o ano a ser analisado: '))
 
 if (ano % 4 == 0 and ano % 100 != 0) or  (ano % 4 == 0 and ano % 400 == 0) :

c8e45e4141739506dc7f51b3f5f706cfe1441fd7
marcelo-moreira-code <contatomarcelomoreira.code@gmail.com>
2022-12-19 11:10:37 -0300

    Update ex032.py
    feito **

--- Exercícios/ex032.py
+++ Exercícios/ex032.py
@@ -4,3 +4,8 @@ from datetime import date
 atual = date.today().year
 print(atual)
 
+ano = int(input('Informe o ano a ser analisado: '))
+if (ano % 4 == 0 and ano % 100 != 0) or  (ano % 4 == 0 and ano % 400 == 0) :
+    print('É ano Bissexto!')
+else:
+    print('Não é ano Bissexto!')    

f93d5a136019f0eb34ac4eac347bd9877908a338
marcelo-moreira-code <contatomarcelomoreira.code@gmail.com>
2022-12-20 23:37:08 -0300

    Update ex036.py
    feito

--- Exercícios/ex036.py
+++ Exercícios/ex036.py
@@ -2,3 +2,12 @@
 # O programa vai perguntar o valor da casa , o salário do comprador e em quantos anos ele vai pagar.
 # Calcule o valor da prestação mensal, sabendo que ela não pode exceder 30% do salário ou então o empréstimo será negado.
 
+valor_da_casa = float(input('Informe o valor da casa em: R$ '))
+salário_comprador = float(input('Quanto ganha mensalmente? R$ '))
+tempo_pag = float(input('Em quantos anos queres pagar a casa: '))
+prestação_mensal = valor_da_casa / (tempo_pag * 12) 
+print(f'{prestação_mensal:.1f}')
+if prestação_mensal > (salário_comprador * 0.3):
+    print('Empréstiomo Recusado! Pois, a prestação mensal não pode exceder 30% do salário do credor.')
+else:
+    print('Seu empréstimo foi aprovado! Meus parabêns!')    

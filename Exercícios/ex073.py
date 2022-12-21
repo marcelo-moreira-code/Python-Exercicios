24c1072024e713e17a3d6c4432442a3e4c24632e
marcelo-moreira-code <contatomarcelomoreira.code@gmail.com>
2022-12-21 14:17:59 -0300

    Update ex073.py
    feito

--- Exercícios/ex073.py
+++ Exercícios/ex073.py
@@ -27,3 +27,9 @@
 19º Avaí
 20º Juventude'''
 
+times = ('1º Palmeiras','2ºInternacional','3º Fluminense','4º Corinthians','5º Flamengo','6º Athletico-PR','7º Atlético-MG','8º Fortaleza','9º São Paulo',
+'10º América-MG','11º Botafogo','12º Santos','13º Goiás','14º Bragantino','15º Coritiba','16º Cuiabá','17º Ceará','18º Atlético-GO','19º Avaí','20º Juventude')
+print(len(times))
+print(f'O 5 primeiros colocados são: {times[0:5]}')
+print(f'Os últimos 4 colocados são: {times[16:21]}')
+print(f'E a posição do São Paulo é {times.index(8)}')

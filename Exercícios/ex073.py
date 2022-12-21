92bf1a8788d2db751deeb611b5913ce9733eb6d2
marcelo-moreira-code <contatomarcelomoreira.code@gmail.com>
2022-12-21 13:25:39 -0300

    Create ex073.py

---
+++ Exercícios/ex073.py
@@ -0,0 +1,29 @@
+# Desafio 073 - Crie uma tupla que mostre os 20 primeiros times do campeonato brasileiro de futebol da séria A,na ordem de colocação.
+# Depois mostre:
+# 1 - Os 5 primeiros times
+# 2 - Os últimos 4 times
+# 3 - Times em ordem alfabética
+# Em que posição está o time do São Paulo
+
+'''CLASSIFICAÇÃO
+1º Palmeiras
+2ºInternacional
+3º Fluminense
+4º Corinthians
+5º Flamengo
+6º Athletico-PR
+7º Atlético-MG
+8º Fortaleza
+9º São Paulo
+10º América-MG
+11º Botafogo
+12º Santos
+13º Goiás
+14º Bragantino
+15º Coritiba
+16º Cuiabá
+17º Ceará
+18º Atlético-GO
+19º Avaí
+20º Juventude'''
+

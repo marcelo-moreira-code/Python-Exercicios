1a2d58c1048712d9c4b669333d3fc3e5cd1ab301
marcelo-moreira-code <contatomarcelomoreira.code@gmail.com>
2022-12-21 10:38:46 -0300

    Create ex072.py

---
+++ Exercícios/ex072.py
@@ -0,0 +1,6 @@
+# Exercício Python 72: Crie um programa que tenha uma dupla totalmente preenchida com uma contagem por extenso, de zero até vinte. Seu programa deverá # ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
+
+números_extenso = ('zero','um','dois','três','quatro','cinco','seis','sete','oito','nove','dez','onze','doze','treze','quatorze','quinze'
+,'dezesseis','dezessete','dezoito','dezenove','vinte')
+
+print(len(números_extenso))
